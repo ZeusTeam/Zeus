469b89966a9233e9e6304e6b87f130fd51856cd5
AngryPowman <138001655@qq.com>
2013-04-26 23:55:29 +0800

    add common.h

---
+++ include/common.h
@@ -0,0 +1,66 @@
+#ifndef ZEUS_COMMON_H_
+#define ZEUS_COMMON_H_
+
+//stl
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <hash_map>
+#include <unordered_map> //c++0x
+
+using namespace std;
+
+//socket dependencies
+#if defined(_WIN32)
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#endif
+
+//debug
+#include <assert.h>
+
+//Datatypes
+#if defined(_WIN32)	//for windows
+#ifndef byte
+typedef unsigned __int8    byte;
+#endif
+typedef __int8             int8;
+typedef __int16            int16;
+typedef __int32            int32;
+typedef __int64            int64;
+typedef unsigned __int32   uint;
+typedef unsigned __int8    uint8;
+typedef unsigned __int16   uint16;
+typedef unsigned __int32   uint32;
+typedef unsigned __int64   uint64;
+#else
+#ifndef byte
+typedef unsigned char      byte;
+#endif
+typedef char               int8;
+typedef short              int16;
+typedef int                int32;
+typedef long long          int64;
+typedef unsigned int       uint;
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+#endif
+
+//Lines
+#if defined(_WIN32)
+#define NEWLINE "\r\n"
+#else
+#define NEWLINE "\n"
+#endif
+
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(x)	if(NULL != (x)) { delete (x); (x)=NULL; }
+#endif
+#endif
