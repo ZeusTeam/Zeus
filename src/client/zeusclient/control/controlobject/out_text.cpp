99e4e85c5ed96c3f21216ea4dd17a83fab840d4e
sum <xukala@qq.com>
2013-04-30 02:16:39 +0800

    回滚

--- src/client/zeusclient/control/controlobject/out_text.cpp
+++ src/client/zeusclient/control/controlobject/out_text.cpp
@@ -1,5 +1,4 @@
 #include "out_text.h"
-#include <cstdio>
 
 int PrintText::m_Pos = 0;
 
@@ -9,20 +8,16 @@ bool PrintText::Print(int x, int y,
         TextRect rect, char* strText, ...)
 {
     //无资源池 暂时这样
-    int nBufferLen = strlen(strText) + 80;
-    char* szBuffer = new char[nBufferLen];
-    memset(szBuffer, 0, nBufferLen);
-
     m_Font = new PrintTextFont("res\\font1.fnt");
     va_list argptr;  //参数列表
     va_start(argptr,strText);
     vsprintf_s(szBuffer , nBufferLen, strText, argptr);
+
     m_Font->printf(x, y, rect, szBuffer);
+
+
+    m_Font->printf(x, y, rect, strText, argptr);
     va_end(argptr);
     delete m_Font;
-    if (szBuffer)
-    {
-        delete[] szBuffer;
-    }
     return true;
 }
