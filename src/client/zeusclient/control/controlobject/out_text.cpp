b88e0a0f8744d7fd766b90e74f304a2c91bdd2da
sum <xukala@qq.com>
2013-04-29 14:53:42 +0800

    加入测试打印类

---
+++ src/client/zeusclient/control/controlobject/out_text.cpp
@@ -0,0 +1,18 @@
+#include "out_text.h"
+
+int PrintText::m_Pos = 0;
+
+PrintTextFont* PrintText::m_Font = NULL;
+
+bool PrintText::Print(int x, int y,
+        TextRect rect, char* strText, ...)
+{
+    //无资源池 暂时这样
+    m_Font = new PrintTextFont("res\\font1.fnt");
+    va_list argptr;  //参数列表
+    va_start(argptr,strText);
+    m_Font->printf(x, y, rect, strText, argptr);
+    va_end(argptr);
+    delete m_Font;
+    return true;
+}
