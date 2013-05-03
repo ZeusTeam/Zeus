b01e0907ecfc7997988bd2e71a9e7ccfa7a8f03a
sum <xukala@qq.com>
2013-05-04 03:23:45 +0800

    加入资源池

---
+++ src/client/zeusclient/control/pool/picture_pool.cpp
@@ -0,0 +1,47 @@
+#include "picture_pool.h"
+
+PicturePool::PicturePool()
+{
+    for (auto it = CPictureXMLParse::Instance()->Begin();
+        it != CPictureXMLParse::Instance()->End();
+        it++)
+    {
+        TextureObject* pObject = new TextureObject;
+        pObject->Load(it->second->PicturePath);
+        m_Pool[it->first] = pObject;
+    }
+}
+
+PicturePool::~PicturePool()
+{
+    Release();
+}
+
+bool PicturePool::Initialize()
+{
+    return false;
+}
+
+TextureObject* PicturePool::Get(const std::string& strId)
+{
+    auto it = m_Pool.find(strId);
+    if (it != m_Pool.end())
+    {
+        return it->second;
+    }
+    return NULL;
+}
+
+void PicturePool::Release()
+{
+    for (auto it = m_Pool.begin();
+        it != m_Pool.end();
+        it++)
+    {
+        if (it->second)
+        {
+            delete it->second;
+            it->second = NULL;
+        }
+    }
+}
