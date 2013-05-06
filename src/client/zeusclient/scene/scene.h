1cbf6b9eb3831fdf9bc9f4c009abac05b9f96082
zapline <278998871@qq.com>
2013-05-06 17:26:13 +0800

    * 笔误

--- src/client/zeusclient/scene/scene.h
+++ src/client/zeusclient/scene/scene.h
@@ -6,7 +6,7 @@ class Scene
 {
 public:
     Scene() {}
-    ///> todo: 这个类和它的子类都要用虚析构函数，否则用delete指向子类的父类指针时会内存泄漏
+    ///> todo: 这个类和它的子类都要用虚析构函数，否则delete指向子类的父类指针时会内存泄漏
     ~Scene() {}
 
     virtual void Update() = 0;
