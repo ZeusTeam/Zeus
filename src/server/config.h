40c789a9f5263cb067c09778f70adaa2a054b378
AngryPowman <138001655@qq.com>
2013-05-21 02:20:34 +0800

    add automake files

---
+++ src/server/config.h
@@ -0,0 +1,23 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "zeus_server"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "138001655@qq.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "zeus_server"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "zeus_server 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "zeus_server"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
